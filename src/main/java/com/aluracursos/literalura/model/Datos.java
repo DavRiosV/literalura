652ae3060ea36819c0578f97b34cd7cd484f86fc
David Rios <david.r.v@hotmail.com>
2025-08-08 02:11:27 -0400

    clase Datos

---
+++ src/main/java/com/aluracursos/literalura/model/Datos.java
@@ -0,0 +1,15 @@
+package com.aluracursos.literalura.model;
+
+import java.util.List;
+
+public class Datos {
+    private List<Libro> results;
+
+    public List<Libro> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Libro> results) {
+        this.results = results;
+    }
+}
